f4a95703b3d46cd7c3d80a26979fd6150e78812b
Khalon Manswell <khalonmanswell@gmail.com>
2026-07-27 20:49:07 -0400

    Trained baseline CNN model

--- src/train_baselineCNN.py
+++ src/train_baselineCNN.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 
-from datasets import train_data_loader
+import datasets
 
 class BaselineCNN(nn.Module):
 
-    def __init__(self, num_classes = 10):
+    def __init__(self, num_classes):
         super().__init__()
 
         self.features = nn.Sequential(
@@ -41,9 +42,76 @@ class BaselineCNN(nn.Module):
 
         return x
 
-images, labels = next(iter(train_data_loader))
 
-model = BaselineCNN()
-outputs = model(images)
-print(images.shape)
-print(outputs.shape)
+def train(model, optimizer, loss_fn, train_loader, val_loader, epochs, device = 'cpu'):
+    model = model.to(device)
+    for epoch in range(epochs):
+        training_loss = 0.0
+        valid_loss = 0.0
+        valid_correct = 0
+
+        model.train()
+
+        for inputs, targets in train_loader:
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+            optimizer.zero_grad()
+
+            output = model(inputs)
+            loss = loss_fn(output, targets)
+
+            loss.backward()
+            optimizer.step()
+
+            training_loss += loss.item() * inputs.size(0)
+        training_loss /= len(train_loader.dataset)
+
+        model.eval()
+        with torch.no_grad():
+            for inputs, targets in val_loader:
+                inputs = inputs.to(device)
+                targets = targets.to(device)
+
+                output = model(inputs)
+                loss = loss_fn(output, targets)
+
+                valid_loss += loss.item()
+
+                predictions = output.argmax(dim = 1)
+                valid_correct += (predictions == targets).sum().item()
+                valid_accuracy = valid_correct / len(val_loader.dataset)
+
+        print(
+            f"Epoch: {epoch+1 }" f"/{epochs} | "
+            f"Train Loss: {training_loss:.4f} | "
+            f"Validation Loss: {valid_loss:.4f} | "
+            f"Validation accuracy: {valid_accuracy:.2%} "
+        )
+
+def get_device():
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+    
+    print(f"Using device: {device}")
+    return device
+
+
+def main():
+    device = get_device()
+
+    model = BaselineCNN(num_classes=10)
+    print(model)
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    loss_fn = nn.CrossEntropyLoss()
+
+    train(model, optimizer, loss_fn, train_loader=datasets.train_data_loader, val_loader=datasets.val_data_loader, 
+          epochs=20, device=device)
+    
+
+if __name__ == "__main__":
+    main()
